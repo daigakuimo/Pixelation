36c7d45cff7feb4442d1a52d87f1cb6ca48a0ede
daigakuimo <enaena128@gmail.com>
2021-11-25 00:18:25 +0900

    feat:outline posteffect

--- Assets/App/Scripts/ShaderHandler.cs
+++ Assets/App/Scripts/ShaderHandler.cs
@@ -6,6 +6,16 @@ using UnityEngine;
 public class ShaderHandler : MonoBehaviour
 {
     public Material effectMaterial;
+
+    private Camera cam;
+
+    private void Start()
+    {
+        //get the camera and tell it to render a depthnormals texture
+        cam = GetComponent<Camera>();
+        cam.depthTextureMode = cam.depthTextureMode | DepthTextureMode.DepthNormals;
+    }
+
     private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         Graphics.Blit(source, destination, effectMaterial);
